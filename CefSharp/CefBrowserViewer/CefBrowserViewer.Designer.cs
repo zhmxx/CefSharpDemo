f8d1d7380cff1f889d4ff6e1291efc22d38d9568
zhmxx <60063176+zhmxx@users.noreply.github.com>
2020-03-20 19:50:08 +0800

    Add address bar

--- CefSharp/CefBrowserViewer/CefBrowserViewer.Designer.cs
+++ CefSharp/CefBrowserViewer/CefBrowserViewer.Designer.cs
@@ -19,33 +19,5 @@
             }
             base.Dispose(disposing);
         }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CefBrowserViewer));
-            this.SuspendLayout();
-            // 
-            // CefBrowserViewer
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoScroll = true;
-            this.AutoScrollMinSize = new System.Drawing.Size(872, 645);
-            this.ClientSize = new System.Drawing.Size(873, 623);
-            //this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "CefBrowserViewer";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            this.ResumeLayout(false);
-
-        }
-
-        #endregion
     }
 }
--- CefSharp/CefBrowserViewer/CefBrowserViewer.cs
+++ CefSharp/CefBrowserViewer/CefBrowserViewer.cs
@@ -25,7 +25,10 @@ namespace Test.Application.Dialogs
             InitializeComponent();
         }
 
+        private TextBox textBox1;
+        private Button button1;
         public ChromiumWebBrowser browser;
+        private const string defaultUrl = "https://www.google.com/";
 
         public void InitBrowser()
         {
@@ -48,7 +51,7 @@ namespace Test.Application.Dialogs
                     //Perform dependency check is to make sure all relevant resources are in our output directory.
                     Cef.Initialize(settings, false, null);
                 }
-                browser = new ChromiumWebBrowser("https://www.google.com/");
+                browser = new ChromiumWebBrowser(defaultUrl);
                 this.Controls.Add(browser);
             }
             catch(Exception ex)
@@ -56,6 +59,51 @@ namespace Test.Application.Dialogs
             }
             
         }//Declare the event method to be called
-       
+
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(545, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = defaultUrl;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(624, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CefBrowserViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoScrollMinSize = new System.Drawing.Size(872, 645);
+            this.ClientSize = new System.Drawing.Size(986, 792);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "CefBrowserViewer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            browser.Load(textBox1.Text);
+        }
     }
 }
